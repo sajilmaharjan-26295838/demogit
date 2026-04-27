82e6c7d71b30fa287f381a62ff8e0cf07b2a5a60
sajilmaharjan-26295838 <sajil.maharjan@student.uts.edu.au>
2026-04-28 01:03:57 +1000

    Initial Commit

---
+++ demogit/demogitApp.swift
@@ -0,0 +1,17 @@
+//
+//  demogitApp.swift
+//  demogit
+//
+//  Created by Sajil Maharjan on 28/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct demogitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
