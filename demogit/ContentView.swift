94befaab187614474ec80e7b8d6ae2c583f08dc9
sajilmaharjan-26295838 <sajil.maharjan@student.uts.edu.au>
2026-05-01 23:25:34 +1000

    my second commit

--- demogit/ContentView.swift
+++ demogit/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "heat.fill")
+            Image(systemName: "heart.fill")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Swiftful things")
+            
+            Button("click me"){
+                
+            }
         }
         .padding()
     }
