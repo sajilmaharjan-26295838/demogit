6195fdeb476c6b98fe287ca1475308e63df69e93
sajilmaharjan-26295838 <sajil.maharjan@student.uts.edu.au>
2026-05-02 17:43:15 +1000

    [Clean] Updates to documentation

--- demogit/ContentView.swift
+++ demogit/ContentView.swift
@@ -6,6 +6,13 @@
 //
 
 /*
+ Clone = Copying the repo locally
+ Commit = Steve ("checkpoint") on our current branch
+ Stage = Prepare changes for a commit
+ Stash = Save changes for later
+ Push = Send local commits to remote repo
+ Pull = Fetch remote comits to local repo
+ 
  COMMIT MESSAGES
  
  NEW FEATURE:
